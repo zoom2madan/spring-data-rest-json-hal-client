df4cfc678db978912f6233e085a269138cff7440
Rashad Asgarbayli <rashad2985@gmail.com>
2019-08-07 10:25:07 +0200

    UPDATE: Versions of the dependencies were raised to the latest ones as of today.

--- webpack.config.js
+++ webpack.config.js
@@ -11,14 +11,14 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
+              test: /\.m?js$/,
+              exclude: /(node_modules|bower_components)/,
+              use: {
+                loader: 'babel-loader',
+                options: {
+                  presets: ['@babel/preset-env']
                 }
-            },
-            {
-                test: /\.html/,
+              }
             }
         ]
     },

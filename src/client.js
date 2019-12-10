53dfeaaeeed0a71e6b4e4943c79fdbe9831442d9
Rashad Asgarbayli <rashad2985@gmail.com>
2019-12-10 20:12:10 +0100

    RELEASE: v2.0.0-BETA2

--- src/client.js
+++ src/client.js
@@ -12,28 +12,31 @@ registry.register('application/json', require('rest/mime/type/application/json')
 registry.register('text/plain', require('rest/mime/type/text/plain'));
 registry.register('text/uri-list', require('./mime/type/text/uri-list'));
 
-const client = rest
+const client = {};
+
+client.request = rest
   .wrap(mime, {registry: registry})
   .wrap(errorCode);
 
-export default client;
 
-export function get(path, headers) {
-  return client({method: "GET", path: path, headers: headers});
-}
+client.get = function(path, headers) {
+  return client.request({method: "GET", path: path, headers: headers});
+};
 
-export function post(path, entity, headers) {
-  return client({method: 'POST', path: path, entity: entity, headers: headers});
-}
+client.post = function(path, entity, headers) {
+  return client.request({method: 'POST', path: path, entity: entity, headers: headers});
+};
 
-export function put(path, entity, headers) {
-  return client({method: 'PUT', path: path, entity: entity, headers: headers});
-}
+client.put = function(path, entity, headers) {
+  return client.request({method: 'PUT', path: path, entity: entity, headers: headers});
+};
 
-export function patch(path, entity, headers) {
-  return client({method: 'PATCH', path: path, entity: entity, headers: headers});
-}
+client.patch = function(path, entity, headers) {
+  return client.request({method: 'PATCH', path: path, entity: entity, headers: headers});
+};
 
-export function del(path, headers) {
-  return client({method: "DELETE", path: path, headers: headers});
-}
+client.del = function(path, headers) {
+  return client.request({method: "DELETE", path: path, headers: headers});
+};
+
+module.exports = client;
